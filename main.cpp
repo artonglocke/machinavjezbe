79233b8bb09a9c0398495da665fa338641a175ed
visualstudiomachina <visualstudiomachina@outlook.com>
2019-05-11 13:29:57 +0200

    Polymorfism

--- main.cpp
+++ main.cpp
@@ -1,136 +1,67 @@
 #include <iostream>
 #include <string>
 #include <vector>
-//Zadatak:
-// Napraviti klasu osobe, sadrzava ime, prezime, adresu, oib
-// Konstruktor treba inicijalizariti sve membere osim oiba
-// Posebna metoda za setanje oiba, provjeriti jel valjan oib ili postojeci, te na temelju toga
-// namjestiti oib
-// Napraviti program koji omogucuje izradu novih osoba i ima mogucnost printanja svih unesenih
-// osoba
 
-class Person
+//Polimorfizam radi samo na heapu!
+
+class Human
 {
 public:
-	Person(std::string givenName, std::string givenLastName, std::string givenAddress)
-	{
-		_name = givenName;
-		_lastName = givenLastName;
-		_address = givenAddress;
-	}
-	
-	bool trySetSSN(std::string ssn)
-	{
-		if (_socialSecurityNumber != "")
-		{
-			std::cout << "Social Security Number is already set!" << std::endl;
-			std::cout << "Persons SSN cannot be changed!" << std::endl;
-			return true;
-		}
+	int hands;
+	int feet;
+	int eyes;
 
-		if (ssn.size() != 11)
-		{
-			std::cout << "Social Security Number is invalid! Wrong number of characters!" << std::endl;
-			return false;
-		}
-
-		for (size_t i = 0; i < ssn.size(); i++)
-		{
-			char current = ssn[i];
-			if (current < 48 || current > 57)
-			{
-				std::cout << "Social Security Number is invalid!" << std::endl;
-				std::cout << "Character at index " << i << " is invalid!" << std::endl;
-				return false;
-			}
-		}
-		_socialSecurityNumber = ssn;
-		return true;
-	}
-
-	void print()
+	virtual void PrintType()
 	{
-		std::cout << "Name: " << _name << " " << _lastName << std::endl;
-		std::cout << "Address: " << _address << std::endl;
-		std::cout << "Social Security Number: " << _socialSecurityNumber << std::endl;
+		std::cout << "I am a human being!" << std::endl;
 	}
 
 private:
-	std::string _name, _lastName, _address, _socialSecurityNumber;
+
 };
 
-void displayPersons(std::vector<Person> persons)
+class Male : public Human
 {
-	for (size_t i = 0; i < persons.size(); i++)
+public:
+	void PrintType()
 	{
-		persons[i].print();
+		std::cout << "I am a male human being!" << std::endl;
 	}
-}
+};
 
-void inputNewData(std::string message, std::string& data)
+class Female : public Human
 {
-	std::cout << message << std::endl;
-	std::cin >> data;
-	std::cin.clear();
-	std::cin.ignore(10000, '\n');
-}
-
-void createPerson(std::vector<Person>& persons)
-{
-	std::string name, lastName, address;
-	inputNewData("Enter new persons name: ", name);
-	inputNewData("Enter new persons last name: ", lastName);
-	inputNewData("Enter new persons address: ", address);
-
-	Person person = Person(name, lastName, address);
-
-	while (true)
+public:
+	void PrintType()
 	{
-		std::string ssn;
-		inputNewData("Enter new social security number: ", ssn);
-		if (person.trySetSSN(ssn))
-		{
-			persons.push_back(person);
-			return;
-		}
+		std::cout << "I am a female human being!" << std::endl;
 	}
-
-	
-}
-
-
+};
 
 int main()
 {
-	std::vector<Person> persons;
+	Male* maleObject = new Male();
+	maleObject->hands = 2;
+	maleObject->feet = 2;
+	maleObject->eyes = 2;
 
-	bool valid = true;
-	while (valid)
+	maleObject->PrintType();
+
+	Female* femaleObject = new Female();
+	femaleObject->PrintType();
+
+	Human* humanObject = new Human();
+
+	std::vector<Human*> humans;
+	humans.push_back(maleObject);
+	humans.push_back(femaleObject);
+	humans.push_back(humanObject);
+
+	std::cout << "======================================" << std::endl;
+
+	for (size_t i = 0; i < humans.size(); i++)
 	{
-		std::cout << "What do you want to do: " << std::endl;
-		std::cout << "1 - Add new person" << std::endl;
-		std::cout << "2 - Exit" << std::endl;
-
-		int choice;
-		std::cin >> choice;
-		std::cin.clear();
-		std::cin.ignore(10000, '\n');
-
-		switch (choice)
-		{
-		case 1:
-			createPerson(persons);
-			break;
-		case 2:
-			valid = false;
-			break;
-		default:
-			break;
-		}
+		humans[i]->PrintType();
 	}
-
-	displayPersons(persons);
-	
-
 	std::cin.get();
 }
